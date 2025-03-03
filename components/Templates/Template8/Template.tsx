ae75201006dbb810adb9ad357fc92663178fdc19
SKB <shubhambhilare4899223@gmail.com>
2025-03-03 21:47:25 +0530

    Template 8 Responsiveness done

--- components/Templates/Template8/Template.tsx
+++ components/Templates/Template8/Template.tsx
@@ -15,42 +15,42 @@ const Template=forwardRef(({
 },ref:React.Ref<HTMLDivElement>) => {
  
   return (
-    <div ref={ref} className="w-[90%] mx-auto bg-white shadow-lg">
+    <div ref={ref} className="w-[90%] min-[425px]:w-[80%] min-[500px]:w-[65%] sm:w-[50%] md:w-full lg:w-[90%] mx-auto bg-white shadow-lg">
       {/* Header */}
       <div className="p-5 text-center ">
-        <h1 className="text-3xl font-medium text-zinc-800 mb-1.5">
+        <h1 className="text-lg xl:text-3xl font-medium text-zinc-800 xl:mb-1.5">
           {PersonalInformationData?.name || "Lorna Alvarado"}
         </h1>
-        <p className="text-lg text-zinc-800">
+        <p className="text-sm xl:text-lg text-zinc-800">
           {PersonalInformationData?.role || "Marketing Manager"}
         </p>
       </div>
 
       {/* Contact Info Banner */}
-      <div className="bg-zinc-200 py-4 px-8 flex justify-between text-sm gap-2 items-center">
+      <div className="bg-zinc-200 py-2.5 xl:py-4 xl:px-8 px-2 min-[425px]:px-4 flex justify-between text-xxs   xl:text-sm gap-2 items-center">
         <div className="flex items-center gap-2 text-zinc-700">
-          <Phone className="w-5 h-5" />
+          <Phone className="xl:w-5 w-3  xl:h-5 h-3" />
           <span>{PersonalInformationData?.phone || "+123-456-7890"}</span>
         </div>
         <div className="flex items-center gap-2 text-zinc-700">
-          <Mail className="w-5 h-5" />
+          <Mail className="xl:w-5 w-3  xl:h-5 h-3" />
           <span>{PersonalInformationData?.email || "hello@reallygreatsite.com"}</span>
         </div>
         <div className="flex items-center gap-2 text-zinc-700">
-          <MapPin className="w-5 h-5" />
+          <MapPin className="xl:w-5 w-3  xl:h-5 h-3" />
           <span>{PersonalInformationData?.address || "123 Anywhere St., Any City"}</span>
         </div>
       </div>
 
-      <div className="grid md:grid-cols-[2fr_1px_3fr] gap-8 p-8">
+      <div className="grid grid-cols-[2fr_1px_3fr]  gap-2 xl:gap-8 xl:p-8 p-4">
         {/* Left Column */}
-        <div className="space-y-8">
+        <div className="space-y-4 xl:space-y-8">
           {/* Education */}
           <section>
-            <h2 className="text-xl font-semibold mb-4 text-zinc-800">EDUCATION</h2>
+            <h2 className="text-xs xl:text-xl font-semibold mb-2 xl:mb-4 text-zinc-800">EDUCATION</h2>
             {EducationData && EducationData[0].degree  ? (
               EducationData.map((item, index) => (
-                <div key={index} className="mb-4 text-sm">
+                <div key={index} className="mb-2 xl:mb-4  text-xxs  xl:text-sm">
                   <h3 className="font-medium text-zinc-800">Degree: {item.degree}</h3>
                   <p className="text-zinc-800">University: {item.university}</p>
                   <p className="text-zinc-500">Location: {item.location}</p>
@@ -59,12 +59,12 @@ const Template=forwardRef(({
               ))
             ) : (
               <>
-                <div className="mb-4 text-sm">
+                <div className="mb-2 xl:mb-4  text-xxs  xl:text-sm">
                   <h3 className="font-medium text-zinc-800">BA Sales and Commerce</h3>
                   <p className="text-zinc-800">Borcelle University</p>
                   <p className="text-zinc-500">(2016 - 2020)</p>
                 </div>
-                <div className="mb-4 text-sm">
+                <div className="mb-2 xl:mb-4  text-xxs  xl:text-sm">
                   <h3 className="font-medium text-zinc-800">Bachelor of Design</h3>
                   <p className="text-zinc-800">Wardiere University</p>
                   <p className="text-zinc-500">(2011 - 2015)</p>
@@ -75,8 +75,8 @@ const Template=forwardRef(({
 
           {/* Skills */}
           <section>
-            <h2 className="text-xl font-semibold mb-4 text-zinc-800">SKILLS</h2>
-            <ul className="space-y-1 text-sm">
+            <h2 className="text-xs xl:text-xl font-semibold mb-2 xl:mb-4 text-zinc-800">SKILLS</h2>
+            <ul className="space-y-1  text-xxs  xl:text-sm">
               {SkillsData && SkillsData[0].data ? (
                 SkillsData.map((item, index) => (
                   <li key={index} className="text-zinc-800">• {item.data}</li>
@@ -97,8 +97,8 @@ const Template=forwardRef(({
 
           {/* Language */}
           <section>
-            <h2 className="text-xl font-semibold mb-4 text-zinc-800">LANGUAGE</h2>
-            <ul className="space-y-1 text-sm">
+            <h2 className="text-xs xl:text-xl font-semibold mb-2 xl:mb-4 text-zinc-800">LANGUAGE</h2>
+            <ul className="space-y-1  text-xxs  xl:text-sm">
               <li className="text-zinc-800">• English</li>
               <li className="text-zinc-800">• Spanish</li>
               <li className="text-zinc-800">• French</li>
@@ -107,14 +107,14 @@ const Template=forwardRef(({
         </div>
 
         {/* Partition Line */}
-        <div className="block w-0.5 bg-zinc-400"></div>
+        <div className="block xl:w-0.5 w-px bg-zinc-400"></div>
 
         {/* Right Column */}
-        <div className="space-y-8 ">
+        <div className="space-y-4 xl:space-y-8 ">
           {/* Profile */}
           <section>
-            <h2 className="text-xl font-semibold mb-4 text-zinc-800">PROFILE</h2>
-            <p className="text-zinc-800 text-sm">
+            <h2 className="text-xs xl:text-xl font-semibold mb-2 xl:mb-4 text-zinc-800">PROFILE</h2>
+            <p className="text-zinc-800  text-xxs  xl:text-sm">
               {PersonalInformationData?.aboutme ||
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sem nec risus egestas accumsan. In enim nunc, tincidunt ut quam eget, luctus sollicitudin neque. Sed leo nisl, semper ac hendrerit a, sollicitudin in arcu."}
             </p>
@@ -122,10 +122,10 @@ const Template=forwardRef(({
 
           {/* Work Experience */}
           <section>
-            <h2 className="text-xl font-semibold mb-4 text-zinc-800">WORK EXPERIENCE</h2>
+            <h2 className="text-xs xl:text-xl font-semibold mb-2 xl:mb-4 text-zinc-800">WORK EXPERIENCE</h2>
             {ExperienceData && ExperienceData[0].company ? (
               ExperienceData.map((item, index) => (
-                <div key={index} className="mb-6 text-sm">
+                <div key={index} className="mb-6  text-xxs  xl:text-sm">
                   <div className="flex justify-between items-start mb-2">
                     <div>
                       <h3 className="font-semibold text-zinc-800">{item.company}</h3>
@@ -140,26 +140,26 @@ const Template=forwardRef(({
                 </div>
               ))
             ) : (
-              <div className="text-sm">
-                <div className="mb-6">
+              <div className= "text-xxs  xl:text-sm">
+                <div className="mb-3 xl:mb-6">
                   <div className="flex justify-between items-start mb-2">
                     <div>
-                      <h3 className="font-medium text-zinc-800">Ginyard International Co.</h3>
+                      <h3 className="font-medium text-zinc-800">Ginyard International</h3>
                       <p className="text-zinc-700">Product Design Manager</p>
                     </div>
-                    <p className="text-zinc-500">2020 - 2023</p>
+                    <p className="text-[6px] xl:text-sm text-zinc-500">2020 - 2023</p>
                   </div>
                   <p className="text-zinc-800">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sem nec risus egestas accumsan. In enim nunc, tincidunt ut quam eget, luctus sollicitudin neque. Sed leo nisl, semper ac hendrerit a, sollicitudin in arcu.
                   </p>
                 </div>
-                <div className="mb-6">
+                <div className="mb-3 xl:mb-6">
                   <div className="flex justify-between items-start mb-2">
                     <div>
                       <h3 className="font-medium text-zinc-800">Liceria & Co.</h3>
                       <p className="text-zinc-700">Product Design Manager</p>
                     </div>
-                    <p className="text-zinc-500">2019 - 2020</p>
+                    <p className="text-[6px] xl:text-sm text-zinc-500">2019 - 2020</p>
                   </div>
                   <p className="text-zinc-800">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sem nec risus egestas accumsan. In enim nunc, tincidunt ut quam eget, luctus sollicitudin neque. Sed leo nisl, semper ac hendrerit a, sollicitudin in arcu.
@@ -171,15 +171,15 @@ const Template=forwardRef(({
 
           {/* References */}
           {/* <section>
-            <h2 className="text-xl font-semibold mb-4 text-zinc-800">REFERENCES</h2>
+            <h2 className="text-xs xl:text-xl font-semibold mb-2 xl:mb-4 text-zinc-800">REFERENCES</h2>
             <div className="grid md:grid-cols-2 gap-8">
-              <div className="text-sm">
+              <div className= "text-xxs  xl:text-sm">
                 <h3 className="font-medium text-zinc-800">Bailey Dupont</h3>
                 <p className="text-zinc-800 text-xs">Wardiere Inc. / CEO</p>
                 <p className="text-zinc-800 text-xs">Phone: 123-456-7890</p>
                 <p className="text-zinc-800 text-xs">Email: hello@reallygreatsite.com</p>
               </div>
-              <div className="text-sm">
+              <div className= "text-xxs  xl:text-sm">
                 <h3 className="font-medium text-zinc-800">Harumi Kobayashi</h3>
                 <p className="text-zinc-800 text-xs">Wardiere Inc. / CEO</p>
                 <p className="text-zinc-800 text-xs">Phone: 123-456-7890</p>
